1f41044ae168edfee1d228433b182081949f825d
gabi-fd <gabi1530.fernandes@gmail.com>
2021-05-26 20:13:02 -0300

    adicionando tabelafluxoDeCaixa e funcionario

---
+++ db/funcionarios.pl
@@ -0,0 +1,23 @@
+:- module(
+    funcionarios, % nome do módulo
+    [ % aqui são colocados os predicados a serem exportados
+      % no formato funtor/aridade.
+      funcionarios/5
+    ]).
+
+% Importa a biblioteca persistency
+:- use_module(library(persistency)).
+
+
+% Esquema da relação funcionarios
+:- persistent
+funcionarios(numfunc: integer,
+      adimissao: date(Date),
+      carteiraTrabalho: integer,
+      ferias: date,
+      horario: integer,
+
+      ).
+
+:- initialization( ( db_attach('./backend/db/tbl_funcionarios.pl', []),
+                 at_halt(db_sync(gc(always))) )).
