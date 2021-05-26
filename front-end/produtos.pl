13ca9edf994c0fd80837fd9a5e7f801a3cde5abf
tayronearaujo <tayrone.machado@zup.com.br>
2021-05-25 22:28:58 -0300

    adicionando tabaela de pessoas e produtos

--- front-end/produtos.pl
+++ front-end/produtos.pl
@@ -1,7 +1,20 @@
 /* html//1, reply_html_page  */
 :- use_module(library(http/html_write)).
+
 /* html_requires  */
 :- use_module(library(http/html_head)).
 
-:- ensure_loaded(base(bootstrap)).
+servidor(Porta) :-
+    http_server(http_dispatch, [port(Porta)]).
+
+:- http_handler(/, oi, []).
+
+oi(_pedido) :-
+    format('content-type: text/plain~n~n'),
+    format('oi mundo !').
+    
+
+
+    
+
 
